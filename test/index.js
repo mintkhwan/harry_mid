45a29e1a75453eeddfd32c5513ddbef2bcecdd94
Mint <mskylinee@hotmail.com>
2016-03-27 03:00:09 +0700

    readme

--- test/index.js
+++ test/index.js
@@ -38,7 +38,7 @@ describe('***BanNaiJinBookStore***', function () {
       case1.should.equal('20')
     })
     it('ห้องแห่งความลับ จำนวน 3 เล่ม, ถ้วยอัคนี จำนวน 3 เล่ม >> ส่วนลด=60 บาท', function * () {
-      var case1 = yield nightmare
+      var case2 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -51,10 +51,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('60')
+      case2.should.equal('60')
     })
     it('ศิลาอาถรรพ์ จำนวน 1 เล่ม, ห้องแห่งความลับ จำนวน 1 เล่ม, ถ้วยอัคนี จำนวน 1 เล่ม >> ส่วนลด=60 บาท', function * () {
-      var case1 = yield nightmare
+      var case3 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -64,10 +64,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('60')
+      case3.should.equal('60')
     })
     it('ศิลาอาถรรพ์ จำนวน 10 เล่ม >> ส่วนลด=0 บาท', function * () {
-      var case1 = yield nightmare
+      var case4 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -84,10 +84,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('0')
+      case4.should.equal('0')
     })
     it('ศิลาอาถรรพ์ จำนวน 2 เล่ม >> ส่วนลด=0 บาท', function * () {
-      var case1 = yield nightmare
+      var case5 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -96,10 +96,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('0')
+      case5.should.equal('0')
     })
     it('ศิลาอาถรรพ์ จำนวน 2 เล่ม, ห้องแห่งความลับ จำนวน 2 เล่ม >> ส่วนลด=40 บาท', function * () {
-      var case1 = yield nightmare
+      var case6 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -110,10 +110,27 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('40')
+      case6.should.equal('40')
+    })
+    it('ศิลาอาถรรพ์ จำนวน 5 เล่ม, ห้องแห่งความลับ จำนวน 1 เล่ม, อัซคาบัน จำนวน 1 เล่ม >> ส่วนลด=60 บาท', function * () {
+      var case7 = yield nightmare
+        .goto('http://localhost:5000')
+        .wait(1000)
+        .click('#b1')
+        .click('#b1')
+        .click('#b1')
+        .click('#b1')
+        .click('#b1')
+        .click('#b2')
+        .click('#b3')
+        .evaluate(function () {
+          this.price = document.querySelector('#mint2').innerHTML
+          return this.price
+        })
+      case7.should.equal('60')
     })
     it('ศิลาอาถรรพ์ จำนวน 1 เล่ม, ห้องแห่งความลับ จำนวน 2 เล่ม, อัซคาบัน จำนวน 3 เล่ม >> ส่วนลด=80 บาท', function * () {
-      var case1 = yield nightmare
+      var case8 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -126,10 +143,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('80')
+      case8.should.equal('80')
     })
     it('ศิลาอาถรรพ์ จำนวน 2 เล่ม, ห้องแห่งความลับ จำนวน 2 เล่ม, อัซคาบัน จำนวน 2 เล่ม >> ส่วนลด=120 บาท', function * () {
-      var case1 = yield nightmare
+      var case9 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -142,10 +159,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('120')
+      case9.should.equal('120')
     })
     it('ศิลาอาถรรพ์ จำนวน 3 เล่ม, ห้องแห่งความลับ จำนวน 3 เล่ม, อัซคาบัน จำนวน 2 เล่ม >> ส่วนลด=140 บาท', function * () {
-      var case1 = yield nightmare
+      var case10 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -160,10 +177,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('140')
+      case10.should.equal('140')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 3 เล่ม, อัซคาบัน จำนวน 3 เล่ม >> ส่วนลด=180 บาท', function * () {
-      var case1 = yield nightmare
+      var case11 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -180,10 +197,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('180')
+      case11.should.equal('180')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 4 เล่ม, อัซคาบัน จำนวน 3 เล่ม >> ส่วนลด=200 บาท', function * () {
-      var case1 = yield nightmare
+      var case12 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -201,10 +218,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('200')
+      case12.should.equal('200')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 4 เล่ม, อัซคาบัน จำนวน 2 เล่ม , ถ้วยอัคนี จำนวน 1 เล่ม >> ส่วนลด=220 บาท', function * () {
-      var case1 = yield nightmare
+      var case13 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -222,10 +239,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('220')
+      case13.should.equal('220')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 4 เล่ม, อัซคาบัน จำนวน 4 เล่ม >> ส่วนลด=240 บาท', function * () {
-      var case1 = yield nightmare
+      var case14 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -244,10 +261,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('240')
+      case14.should.equal('240')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 4 เล่ม, อัซคาบัน จำนวน 3 เล่ม, ถ้วยอัคนี จำนวน 1 เล่ม >> ส่วนลด=260 บาท', function * () {
-      var case1 = yield nightmare
+      var case15 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -266,10 +283,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('260')
+      case15.should.equal('260')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 3 เล่ม, อัซคาบัน จำนวน 3 เล่ม, ถ้วยอัคนี จำนวน 2 เล่ม >> ส่วนลด=300 บาท', function * () {
-      var case1 = yield nightmare
+      var case16 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -288,10 +305,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('300')
+      case16.should.equal('300')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 3 เล่ม, อัซคาบัน จำนวน 3 เล่ม, ถ้วยอัคนี จำนวน 3 เล่ม >> ส่วนลด=360 บาท', function * () {
-      var case1 = yield nightmare
+      var case17 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -311,10 +328,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('360')
+      case17.should.equal('360')
     })
     it('ศิลาอาถรรพ์ จำนวน 4 เล่ม, ห้องแห่งความลับ จำนวน 4 เล่ม, อัซคาบัน จำนวน 3 เล่ม, ถ้วยอัคนี จำนวน 3 เล่ม >> ส่วนลด=380 บาท', function * () {
-      var case1 = yield nightmare
+      var case18 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -335,10 +352,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('380')
+      case18.should.equal('380')
     })
     it('ศิลาอาถรรพ์, ห้องแห่งความลับ, อัซคาบัน, ถ้วยอัคนี, นกฟีนิกซ์, เจ้าชายเลือดผสม, เครื่องรางยมทูต ภาคละ 1 เล่ม >> ส่วนลด=420 บาท', function * () {
-      var case1 = yield nightmare
+      var case19 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -352,10 +369,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('420')
+      case19.should.equal('420')
     })
     it('ศิลาอาถรรพ์, ห้องแห่งความลับ, อัซคาบัน, ถ้วยอัคนี, นกฟีนิกซ์, เจ้าชายเลือดผสม, เครื่องรางยมทูต ภาคละ 2 เล่ม >> ส่วนลด=840 บาท', function * () {
-      var case1 = yield nightmare
+      var case20 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -376,10 +393,10 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('840')
+      case20.should.equal('840')
     })
     it('ศิลาอาถรรพ์, ห้องแห่งความลับ, อัซคาบัน, ถ้วยอัคนี, นกฟีนิกซ์, เจ้าชายเลือดผสม, เครื่องรางยมทูต ภาคละ 3 เล่ม >> ส่วนลด=1260 บาท', function * () {
-      var case1 = yield nightmare
+      var case21 = yield nightmare
         .goto('http://localhost:5000')
         .wait(1000)
         .click('#b1')
@@ -407,7 +424,7 @@ describe('***BanNaiJinBookStore***', function () {
           this.price = document.querySelector('#mint2').innerHTML
           return this.price
         })
-      case1.should.equal('1260')
+      case21.should.equal('1260')
     })
   })
 })
